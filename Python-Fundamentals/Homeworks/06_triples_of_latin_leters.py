cc4447d1c8ceb0b401aa0bf4bacc9d8a7f2e948f
Simeon Trenev <75264450+setrenev@users.noreply.github.com>
2021-01-31 20:20:42 +0200

    Delete 06_triples_of_latin_leters.py

--- Python-Fundamentals/Homeworks/06_triples_of_latin_leters.py
+++
@@ -1,6 +0,0 @@
-number = int(input())
-
-for char_1 in range(97, 97 + number):
-    for char_2 in range(97, 97 + number):
-        for char_3 in range(97, 97 + number):
-            print(f"{chr(char_1)}{chr(char_2)}{chr(char_3)}")
